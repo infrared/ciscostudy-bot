fcc2c829f6d9889af1f383b3d5d8b331f58e54cd
Michael Kroher <michael@kroher.net>
2014-01-25 18:37:26 -0500

    first

---
+++ README.md
@@ -0,0 +1,32 @@
+#####Bot Commands#####
+
+######IP Calculations######
+
+***binary***
+
+    <user> 1010
+    <bot> {"dec":10,"hex":"a"}
+
+***hex***
+
+    <user> 0xFF
+    <bot> {"dec":255,"bin":"11111111"}
+    
+
+######Quiz######
+
+***View available certs***
+
+    <user> certs
+    <bot> icnd1, icnd2, ...
+    
+
+***Count cert database entries***
+Syntax: count <cert name>
+
+Example:
+    
+    <user> count icnd1
+    <bot> 354
+    
+
---
+++ bot.js
@@ -0,0 +1,49 @@
+var irc = require('irc');
+
+var config = require('./config.json');
+
+var modules = { };
+modules.conversion = require('./lib/conversion.js');
+modules.ciscostudy = require('./lib/ciscostudy.js');
+
+var client = new irc.Client('irc.freenode.net',
+    config.nick,
+    {
+        channels: config.channels
+    }
+);
+
+
+client.addListener('message',function(from,to,message) {
+    console.log({ "from": from, "to": to});
+    for (var i=0;i<config.dispatch.length;i++) {
+        var re = new RegExp(config.dispatch[i].match);
+        if (re.test(message)) {
+            console.log("matched: ",re);
+            if (config.dispatch[i].say !== undefined) {
+                client.say(to, config.dispatch[i].say);
+            } else if ( config.dispatch[i].module !== undefined) {
+                var module = config.dispatch[i].module;
+                var func   = config.dispatch[i].func;
+                var args = message.match(re);
+                var data = {
+                    from: from,
+                    to : to,
+                    message: message,
+                    args: args
+                };
+                try {
+                    modules[module][func](data,function(err,res) { 
+                        client.say(to,res);
+                    });
+                } catch(err) {
+                    client.say(to,err);
+                }
+                break; 
+            }
+        }
+    }
+});
+client.addListener('error',function(message) {
+    console.log('error: ',message);
+});
